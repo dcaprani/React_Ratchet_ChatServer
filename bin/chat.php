fe3a05d875991edd96eb59d37e961dac1ffeeb17
Derek Caprani <dcaprani@me.com>
2015-11-30 08:52:32 +0000

    30/11/2015 Commitet update

--- bin/chat.php
+++ bin/chat.php
@@ -3,7 +3,7 @@
 
 require_once __DIR__."/../vendor/autoload.php";
 
-$ip = (isset($argv[1])) ? $argv[1] : "127.0.0.1";
+$ip = (isset($argv[1])) ? $argv[1] : "10.0.2.15";
 $hostname = (isset($argv[2])) ? $argv[2] : "localhost";
 
 
@@ -18,10 +18,10 @@ $http_socket->listen(8000, $ip);
 echo "Web server active at http://localhost:8000/\n";
 
 
-$app = new Ratchet\App($hostname, 8080, $ip, $loop);
+$app = new Ratchet\App($hostname, 5050, $ip, $loop);
 $app->route("/", new Demo\Chat());
 
-echo "Chat server active at ws://localhost:8080/\n";
+echo "Chat server active at ws://localhost:5050/\n";
 
 $app->run();
 
